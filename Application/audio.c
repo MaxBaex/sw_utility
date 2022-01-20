b3f29b0b9d2bb324b14d107bb5489a1921eeffca
Maximilian Betz <Maximilian.Betz@awi.de>
2022-01-20 19:21:10 +0100

    tested frequency change

--- Application/audio.c
+++ Application/audio.c
@@ -20,6 +20,21 @@ uint32_t Wave_LUT[NS] = {
 };
 
 
+//With some harmonics and a step in between:
+uint32_t Wave_LUT_1[NS] = {
+  518 , 522 , 698 , 933 , 990 , 866 , 787 , 853 , 886 , 712 , 445 , 325 , 359 , 328 ,
+  144 , 0 , 77 , 268 , 341 , 297 , 355 , 600 , 829 , 856 , 781 , 825 , 972 , 991 , 796 ,
+  585 , 541 , 562 , 434 , 184 , 52 , 133 , 238 , 195 , 117 , 224 , 487 , 664 , 659 , 656 ,
+  814 , 1006 , 1006 , 832 , 719 , 755 , 751 , 550 , 286 , 193 , 255 , 250 , 103 , 15 , 149 ,
+  376 , 467 , 438 , 513 , 753 , 950 , 930 , 815 , 824 , 926 , 892 , 658 , 435 , 396 , 421 ,
+  303 , 85 , 7 , 142 , 285 , 274 , 235 , 377 , 650 , 810 , 779 , 752 , 880 , 1022 , 961 ,
+  742 , 605 , 622 , 598 , 389 , 147 , 96 , 199 , 227 , 122 , 87 , 270 , 520 , 612 , 583 , 655 ,
+  874 , 1023 , 947 , 790 , 766 , 829 , 754 , 497 , 281 , 263 , 309 , 215 , 41 , 22 , 209 , 383 ,
+  394 , 380 , 539 , 803 , 928 , 856 , 797
+};
+
+
+
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim4;
 extern DMA_HandleTypeDef hdma_tim4_ch1;
@@ -58,10 +73,49 @@ void StartAudioTask(void *argument)
    * highest volume = 0x000000XX
    *
    */
+  HAL_GPIO_WritePin(GPIOA, ATTN_1_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, ATTN_2_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, ATTN_3_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, ATTN_4_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, ATTN_5_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, ATTN_6_Pin, GPIO_PIN_RESET);
+
+  for(;;)
+   {
+
+
+
+
+
+      HAL_TIM_OC_Stop(&htim4, TIM_CHANNEL_1);
+      HAL_TIM_OC_Stop(&htim4, TIM_CHANNEL_2);
+      osDelay(250);
+
+      htim4.Init.Period =  TIM4CLK / (NS * 1000); - 1;
+      HAL_TIM_Base_Init(&htim4);
+      HAL_TIM_OC_Start(&htim4, TIM_CHANNEL_1);
+      HAL_TIM_OC_Start(&htim4, TIM_CHANNEL_2);
+      osDelay(250);
+
+      HAL_TIM_OC_Stop(&htim4, TIM_CHANNEL_1);
+      HAL_TIM_OC_Stop(&htim4, TIM_CHANNEL_2);
+      osDelay(250);
+
+      htim4.Init.Period =  TIM4CLK / (NS * 2000); - 1;
+      HAL_TIM_Base_Init(&htim4);
+      HAL_TIM_OC_Start(&htim4, TIM_CHANNEL_1);
+      HAL_TIM_OC_Start(&htim4, TIM_CHANNEL_2);
+      osDelay(250);
+
+   }
+
 
 
   for(;;)
   {
+
+
+
 
 
     HAL_GPIO_WritePin(GPIOA, ATTN_1_Pin, GPIO_PIN_RESET);
