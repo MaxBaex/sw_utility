8c8868daae1d5ff757ff838feba7ea03c6636cd1
Maximilian Betz <Maximilian.Betz@awi.de>
2022-01-25 20:50:50 +0100

    cleaned up sensing.c

--- sw/Application/sensing.c
+++ sw/Application/sensing.c
@@ -9,16 +9,10 @@
 
 
 extern I2C_HandleTypeDef hi2c1;
-
-
-/******************************************************************************/
-/*!                Static variable definition                                 */
 static uint8_t dev_addr;
 
 
-/*!
- * I2C read function map
- */
+/*! I2C read function map */
 BME68X_INTF_RET_TYPE bme68x_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
 {
   uint8_t dev_addr = *(uint8_t*)intf_ptr;
@@ -33,9 +27,7 @@ BME68X_INTF_RET_TYPE bme68x_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32
     }
 }
 
-/*!
- * I2C write function map
- */
+/*! I2C write function map */
 BME68X_INTF_RET_TYPE bme68x_i2c_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr)
 {
   uint8_t dev_addr = *(uint8_t*)intf_ptr;
@@ -50,9 +42,7 @@ BME68X_INTF_RET_TYPE bme68x_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
     }
 }
 
-/*!
- * Delay function map to FreeRTOS. Delay below 1ms will be rounded up to 1ms.
- */
+/*! Delay function map to FreeRTOS. Delay below 1ms will be rounded up to 1ms. */
 void bme68x_delay_us(uint32_t period, void *intf_ptr)
 {
   if (period < 1000)
@@ -63,38 +53,6 @@ void bme68x_delay_us(uint32_t period, void *intf_ptr)
     {
       vTaskDelay(period / 1000);
     }
-}
-
-void bme68x_check_rslt(const char api_name[], int8_t rslt)
-{
-  switch (rslt)
-  {
-    case BME68X_OK:
-
-      /* Do nothing */
-      break;
-    case BME68X_E_NULL_PTR:
-      Error_Handler(); //Error
-      break;
-    case BME68X_E_COM_FAIL:
-      Error_Handler(); //Communication failure
-      break;
-    case BME68X_E_INVALID_LENGTH:
-      Error_Handler(); //Incorrect length parameter
-      break;
-    case BME68X_E_DEV_NOT_FOUND:
-      Error_Handler(); //Device not found
-      break;
-    case BME68X_E_SELF_TEST:
-      Error_Handler(); //Self test error
-      break;
-    case BME68X_W_NO_NEW_DATA:
-      Error_Handler(); //No new data found
-      break;
-    default:
-      Error_Handler(); //Unknown error code
-      break;
-  }
 }
 
 int8_t bme68x_interface_init(struct bme68x_dev *bme, uint8_t intf)
@@ -150,26 +108,22 @@ void StartSensingTask(void *argument)
     {
       if (BME68X_OK == bme68x_init(&bme))
 	{
-	  /* Check if rslt == BME68X_OK, report or handle if otherwise */
 	  conf.filter = BME68X_FILTER_OFF;
 	  conf.odr = BME68X_ODR_NONE;
 	  conf.os_hum = BME68X_OS_16X;
 	  conf.os_pres = BME68X_OS_16X;
 	  conf.os_temp = BME68X_OS_16X;
 
-
 	  if (BME68X_OK == bme68x_set_conf(&conf, &bme))
 	    {
-	      /* Check if rslt == BME68X_OK, report or handle if otherwise */
 	      heatr_conf.enable = BME68X_DISABLE;   /* Enabling this causes to high temperature values for quick consecutive readings*/
 	      heatr_conf.heatr_temp = 300;
 	      heatr_conf.heatr_dur = 100;
 
-
 	      if (BME68X_OK == bme68x_set_heatr_conf(BME68X_FORCED_MODE, &heatr_conf, &bme))
 		{
 
-		  for(;;)
+		  for(;;)  /* Enter cyclic measurement mode here */
 		    {
 		      if(BME68X_OK == bme68x_set_op_mode(BME68X_FORCED_MODE, &bme))
 			{
@@ -180,7 +134,7 @@ void StartSensingTask(void *argument)
 			  /* Check if rslt == BME68X_OK, report or handle if otherwise */
 			  if(BME68X_OK == bme68x_get_data(BME68X_FORCED_MODE, &data, &n_fields, &bme))
 			    {
-			      /* Only send data if measurement was successful*/
+			      /* Only send data if measurement was successful */
 			      rawData.id = c_CAN_Id_Temperature;
 			      rawData.value = data.temperature;
 			      if( pdTRUE != xQueueSend(COM_Message_Queue, &rawData, 0))
@@ -205,21 +159,21 @@ void StartSensingTask(void *argument)
 			    }
 			  else
 			    {
-			      /* Some error happened. Break loop and reinitialize all*/
+			      /* Some error happened. Break loop and reinitialize everything from scratch */
 			      break;
 			    }
 
 			}
 		      else
 			{
-			  /* Some error happened. Break loop and reinitialize all*/
+			  /* Some error happened. Break loop and reinitialize everything from scratch */
 			  break;
 			}
 		    }
 		}
 	    }
 	}
-      /* Wait some time and try again if any error happens*/
+      /* Wait some time and try again if any error happens */
       vTaskDelay(500);
     }
 }
